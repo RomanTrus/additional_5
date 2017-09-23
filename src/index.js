ff4d37293821b51b232f9fd5c682d212b863e382
Roman Trus <radadendronroma@gmail.com>
2017-09-23 16:41:48 +0300

    additional_5/feature: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+		length1=str.length;
+		length2=str.length-1;
+		while (length2<length1){
+			length1=str.length;
+ 	for (var i = 0; i<bracketsConfig.length; i++) {
+ 		while (str.indexOf(bracketsConfig[i][0]+bracketsConfig[i][1])>=0){
+ 			str= str.slice(0,str.indexOf(bracketsConfig[i][0]+bracketsConfig[i][1]))+str.slice(str.indexOf(bracketsConfig[i][0]+bracketsConfig[i][1])+2)
+ 		}
+ 		length2=str.length;
+ 	}
+ 	}
+ 	if (!str.length) {return true} else {return false}
+ }
